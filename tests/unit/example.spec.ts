0c103eb504469aa91b96944d9c447e16cd1c951b
jpon0113 <jpongama@gamil.com>
2025-05-22 13:23:54 +0800

    feat: jest practice for tirgger, setValue

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -9,6 +9,31 @@ describe('HelloWorld.vue', () => {
 		const wrapper = shallowMount(HelloWorld, {
 			props: { msg },
 		});
-		console.log(wrapper.findComponent(TemplateList));
+		expect(wrapper.get('h1').text()).toBe(msg);
+		expect(wrapper.findComponent(Hello).exists()).toBeTruthy();
+	});
+	it('should update the count when clicking the button', async () => {
+		const msg = 'new message';
+		const wrapper = shallowMount(HelloWorld, {
+			props: { msg },
+		});
+		await wrapper.get('button').trigger('click');
+		expect(wrapper.get('button').text()).toBe('2');
+	});
+	it('should add todo when fill the input and click the add button', async () => {
+		const msg = 'new message';
+		const todoContent = 'buy milk';
+		const wrapper = shallowMount(HelloWorld, {
+			props: { msg },
+		});
+		await wrapper.get('input').setValue(todoContent);
+		expect(wrapper.get('input').element.value).toBe(todoContent);
+		await wrapper.get('.addTodo').trigger('click');
+		expect(wrapper.findAll('li')).toHaveLength(1);
+		expect(wrapper.get('li').text()).toBe(todoContent);
+		console.log(wrapper.emitted());
+		expect(wrapper.emitted()).toHaveProperty('send');
+		const events = wrapper.emitted('send');
+		expect(events[0]).toEqual([todoContent]);
 	});
 });
